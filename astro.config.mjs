48b36423b6c5e3db471e6f951418634825c9d176
Michael Funk <funkstyr@gmail.com>
2025-06-08 11:00:47 -0400

    feat: add biome and astro plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+
+import { expressiveCodeOptions } from "./src/site.config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: process.env.SITE_URL,
+  prefetch: true,
+  output: "server",
+
+  integrations: [
+    expressiveCode(expressiveCodeOptions),
+    icon(),
+    mdx(),
+    sitemap(),
+  ],
 });

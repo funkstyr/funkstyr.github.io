81af1da804cdabe80bf97e4d710e4595afab4008
funkstyr <funkstyr@gmail.com>
2025-12-06 10:22:03 -0800

    feat: add PDF view of resume

--- src/site.config.ts
+++ src/site.config.ts
@@ -54,7 +54,7 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
     if (styleVariants.length >= 2) {
       const baseTheme = styleVariants[0]?.theme;
       const altTheme = styleVariants.find(
-        (v) => v.theme.type !== baseTheme?.type
+        (v) => v.theme.type !== baseTheme?.type,
       )?.theme;
       if (theme === baseTheme || theme === altTheme)
         return `[data-theme='${theme.type}']`;
